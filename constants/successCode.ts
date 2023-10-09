090f2d5c3eae7f370ceee767c312bef3260b3ac7
mohi <mohi@flivv.com>
2023-10-10 00:24:50 +0530

    configured constants for error & success messages

---
+++ constants/successCode.ts
@@ -0,0 +1,18 @@
+const COMMON = {};
+
+const AUTH = {
+  EMAIL_SENT: "Please verify your email address ",
+  EMAIL_VERIFICATION_SUCCESS: "Email Verified. Please continue to login.",
+  ACCOUNT_CREATED: "Account Created Successfully. Please login to continue",
+};
+
+const USER = {
+  PROFILE_UDPATE_SUCCESS: "Profile Updated",
+  PASSWORD_CHANGED_SUCCESS: "Password Changed Successfully",
+};
+
+export const SUCCESS_CODES = {
+  COMMON,
+  AUTH,
+  USER,
+};
