6f01c7b08530a8e13aa01e47f14906b5c6bfcda7
mohi <mohi@flivv.com>
2023-10-12 21:23:24 +0530

    added /health check route

--- constants/errorCodes.ts
+++ constants/errorCodes.ts
@@ -1,31 +1,30 @@
 const COMMON = {
-    MISSING_PARAMETERS: "Missing parameters",
-    INTERNAL_SERVER_ERROR: "Internal server error",
-    UNAUTHORIZED: "Unauthorized",
-    EMAIL_VERIFICATION: "Unable to verfiy email address.",
-  };
-  
-  const USER = {
-    NOT_FOUND: "User not found",
-    ALREADY_EXISTS: "User already exits.",
-    NOT_AUTHORIZED: "User not authorized",
-    NOT_AUTHENTICATED: "User not authenticated",
-    INCORRECT_CREDENTIALS: "Invalid Username & Password",
-    PASSWORD_CHANGE_ERROR: "Failed to change password",
-    PASSWORD_VALIDATION: "Old Password Incorrect",
-  };
-  
-  const AUTH = {
-    ACCESS_TOKEN: "Access Denied, No Token Provided",
-    SEND_EMAIL_SUCCESS: "Email sent successfully",
-    SEND_EMAIL_FAILED: "Fail to sent email",
-    INVALID_EMAIL: "Invalid Email",
-    INVALID_PASSWORD: "Invalid Password",
-  };
-  
-  export const ERROR_CODES = {
-    COMMON,
-    USER,
-    AUTH,
-  };
-  
+  MISSING_PARAMETERS: "Missing parameters",
+  INTERNAL_SERVER_ERROR: "Internal server error",
+  UNAUTHORIZED: "Unauthorized",
+  EMAIL_VERIFICATION: "Unable to verfiy email address.",
+};
+
+const USER = {
+  NOT_FOUND: "User not found",
+  ALREADY_EXISTS: "User already exits.",
+  NOT_AUTHORIZED: "User not authorized",
+  NOT_AUTHENTICATED: "User not authenticated",
+  INCORRECT_CREDENTIALS: "Invalid Username & Password",
+  PASSWORD_CHANGE_ERROR: "Failed to change password",
+  PASSWORD_VALIDATION: "Old Password Incorrect",
+};
+
+const AUTH = {
+  ACCESS_TOKEN: "Access Denied, No Token Provided",
+  SEND_EMAIL_SUCCESS: "Email sent successfully",
+  SEND_EMAIL_FAILED: "Fail to sent email",
+  INVALID_EMAIL: "Invalid Email",
+  INVALID_PASSWORD: "Invalid Password",
+};
+
+export const ERROR_CODES = {
+  COMMON,
+  USER,
+  AUTH,
+};
--- src/routes/health.ts
+++ src/routes/health.ts
@@ -4,8 +4,7 @@ import express from "express";
 const router = express.Router();
 
 router.get("/", async (req: Request, res: Response) => {
-    return res.json({ok:true,message:"Server is up & running"})
+  return res.json({ ok: true, message: "Server is up & running" });
 });
-
 
 export default router;
